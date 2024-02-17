a704c0863469c246f5bcdbdb4a443057e8ba0849
ciherrera20 <ciherrera20@icloud.com>
2024-02-16 23:26:38 -0800

    Debugging action

--- sql/09.sql
+++ sql/09.sql
@@ -2,4 +2,4 @@
  * Use a JOIN to display the total amount rung up by each staff member in January of 2020.
  * Use tables staff and payment.
  */
-SELECT first_name, last_name, sum(amount) FROM staff JOIN payment USING (staff_id) WHERE payment_date >= '31-DEC-2019' AND payment_date <= '1-FEB-2020' GROUP BY staff_id;
+SELECT first_name, last_name, sum(amount) FROM staff JOIN payment USING (staff_id) WHERE payment_date >= '31-DEC-2019' AND payment_date <= '1-FEB-2020' GROUP BY staff_id ORDER BY first_name;
